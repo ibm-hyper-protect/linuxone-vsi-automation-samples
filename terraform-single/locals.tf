6f69a9c184f9707a4c606a0e64487c37509caa77
Angel Nunez Mencias <anunez@de.ibm.com>
2022-08-01 19:48:52 +0200

    change z/OS default image

--- terraform-single/locals.tf
+++ terraform-single/locals.tf
@@ -1,5 +1,5 @@
 locals {
   full_zone = "${var.region}-${var.zone}"
   profile    = var.profile    != null ? var.profile    : (var.os_type=="zos" ? "mz2-2x16"      : "bz2-2x8")
-  image_name = var.image_name != null ? var.image_name : (var.os_type=="zos" ? ".*zos.*stock.*" : ".*ubuntu.*s390x.*")
+  image_name = var.image_name != null ? var.image_name : (var.os_type=="zos" ? ".*zos.*" : ".*ubuntu.*s390x.*")
 }
