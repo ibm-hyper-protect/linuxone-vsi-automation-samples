e417c9a17ff41ef22c1e124f62745ebd4076d3ae
Vishwanatha Subbanna <vishwabmc@users.noreply.github.com>
2022-09-14 15:51:31 +0530

    Cleanup few issues and update variables.tf (#18)
    * Cleanup few issues and update variables.tf
    
    Restructured the description section in variables.tf, and removed some
    text which were not applicable.
    
    Signed-off-by: vishwabmc <vishwanath@in.ibm.com>
    
    * Fix review comments
    
    Signed-off-by: vishwabmc <vishwanath@in.ibm.com>
    
    Signed-off-by: vishwabmc <vishwanath@in.ibm.com>
    Co-authored-by: vishwabmc <vishwanath@in.ibm.com>

--- terraform-hpvs/nginx-hello/variables.tf
+++ terraform-hpvs/nginx-hello/variables.tf
@@ -1,36 +1,69 @@
 variable "ibmcloud_api_key" {
-  description = "Enter your IBM Cloud API Key, you can get your IBM Cloud API key using: https://cloud.ibm.com/iam#/apikeys"
+  description = <<-DESC
+                  Enter your IBM Cloud API Key, you can get your IBM Cloud API key using:
+                   https://cloud.ibm.com/iam#/apikeys
+                DESC
+  sensitive  = true
 }
 
 variable "region" {
   type        = string
-  description = "Region to deploy to, e.g. eu-gb."
+  description = "Region to deploy to, e.g. eu-gb"
+
+   validation {
+    condition     = ( var.region == "eu-gb"  ||
+                      var.region == "br-sao" ||
+                      var.region == "ca-tor" ||
+                      var.region == "jp-tok" ||
+                      var.region == "us-east" )
+    error_message = "Value of region must be one of eu-gb/br-sao/ca-tor/jp-tok/us-east."
+  }
 }
 
 variable "zone" {
   type        = string
+  default     = "2"
   description = "Zone to deploy to, e.g. 2."
+
+  validation {
+    condition     = ( var.zone == "1" ||
+                      var.zone == "2" ||
+                      var.zone == "3")
+    error_message = "Value of zone must be one of 1/2/3."
+  }
 }
 
 variable "logdna_ingestion_key" {
   type        = string
-  description = "Ingestion key for logDNA."
   sensitive   = true
+  description = <<-DESC
+                  Ingestion key for IBM Log Analysis instance. This can be 
+                  obtained from "Linux/Ubuntu" section of "Logging resource" 
+                  tab of IBM Log Analysis instance
+                DESC
 }
 
 variable "logdna_ingestion_hostname" {
   type        = string
-  description = "Ingestion hostname (just the name not the port)."
+  description = <<-DESC
+                  rsyslog endpoint of IBM Log Analysis instance. 
+                  Don't include the port. Example: 
+                  syslog-a.<log_region>.logging.cloud.ibm.com
+                  log_region is the region where IBM Log Analysis is deployed
+                DESC
 }
 
 variable "prefix" {
   type        = string
-  description = "Prefix for all generated resources. Make sure to have a custom image with that name."
   default     = "hpcr-sample-nginx-hello"
+  description = "Prefix to be attached to name of all generated resources"
 }
 
 variable "profile" {
   type        = string
-  description = "Profile used for the VSI, this has to be a secure execution profile in the format Xz2e-YxZ, e.g. bz2e-1x4."
   default     = "bz2e-1x4"
+  description = <<-DESC
+                  Profile used for the VSI. This has to be a secure execution 
+                  profile in the format Xz2e-YxZ, e.g. bz2e-1x4
+                DESC
 }
